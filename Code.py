32a00486c85efd5b127593acc9ba7fc9f2192f82
Venkatesh Avula <42148392+venkateshavula@users.noreply.github.com>
2018-12-18 16:55:28 -0500

    Update Code.py

--- Code.py
+++ Code.py
@@ -6,3 +6,10 @@ from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer, VectorAsse
 from pyspark.ml.regression import RandomForestRegressor, GBTRegressor, DecisionTreeRegressor, GeneralizedLinearRegression
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+
+#Read data
+df =sparkSession.sql("SELECT * FROM dataframe")
+
+# Lists for numeric and categorical features
+numericCols = [c for c, t in df.dtypes if t not in ('string','date')]
+categoricalCols  = df.select(list(set(df.columns)-set(num))).columns
